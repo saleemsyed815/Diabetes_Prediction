b4a9c4416f1c225b2322acb5b2c50c7bf0e562d5
Syed Saleemuddin <saleemsyed815@gmail.com>
2022-06-30 16:20:12 +0530

    used random forest to increase the accuracy to 98%

--- DiabetesPrediction/DiabetesPrediction/views.py
+++ DiabetesPrediction/DiabetesPrediction/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
+
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
 
 def home(request):
     return render(request, 'home.html')
@@ -13,33 +13,54 @@ def predict(request):
     return render(request, 'predict.html')
 
 def result(request):
-    data = pd.read_csv(r'C:/Users/Admin/Desktop/diabetes/diabetes.csv')
+    data = pd.read_csv(r'C:/Users/Admin/Desktop/diabetes/kaggle_diabetes.csv')
 
-    X = data.drop("Outcome", axis= 1)
-    y = data["Outcome"]
+    dataset_new = dataset
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+    #replacing 0 values with Nan
+    dataset_new[["Glucose", "BloodPressure", "SkinThickness", "Insulin", "BMI"]] = dataset_new[["Glucose", "BloodPressure", "SkinThickness", "Insulin", "BMI"]].replace(0, np.NaN)
 
-    model = LogisticRegression()
-    model.fit(X_train, y_train)
+    # Replacing NaN with mean values
+    dataset_new["Glucose"].fillna(dataset_new["Glucose"].mean(), inplace = True)
+    dataset_new["BloodPressure"].fillna(dataset_new["BloodPressure"].mean(), inplace = True)
+    dataset_new["SkinThickness"].fillna(dataset_new["SkinThickness"].mean(), inplace = True)
+    dataset_new["Insulin"].fillna(dataset_new["Insulin"].mean(), inplace = True)
+    dataset_new["BMI"].fillna(dataset_new["BMI"].mean(), inplace = True)
+
+    # Feature scaling using MinMaxScaler
+    from sklearn.preprocessing import MinMaxScaler
+    sc = MinMaxScaler(feature_range = (0, 1))
+    dataset_scaled = sc.fit_transform(dataset_new)
+
+    dataset_scaled = pd.DataFrame(dataset_scaled)
+
+    # Selecting features - [Glucose, Insulin, BMI, Age]
+    X = dataset_scaled.iloc[:, [1, 4, 5, 7]].values
+    Y = dataset_scaled.iloc[:, 8].values
+
+    # Splitting X and Y
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.20, random_state = 42, stratify = dataset_new['Outcome'] )
+
+    # Random forest Algorithm
+    from sklearn.ensemble import RandomForestClassifier
+    ranforest = RandomForestClassifier(n_estimators = 11, criterion = 'entropy', random_state = 42)
+    ranforest.fit(X_train, Y_train)
+
+    # prediction = ranforest.predict(X_test)
 
     val1 = float(request.GET['n1'])
     val2 = float(request.GET['n2'])
     val3 = float(request.GET['n3'])
     val4 = float(request.GET['n4'])
-    val5 = float(request.GET['n5'])
-    val6 = float(request.GET['n6'])
-    val7 = float(request.GET['n7'])
-    val8 = float(request.GET['n8'])
 
-    pred = model.predict([[val1, val2, val3, val4, val5, val6, val7, val8]])
+    prediction = ranforest.predict([[val1, val2, val3, val4]])
 
     result1 = ""
-    if pred==[1]:
+    if prediction==[1]:
         result1 = "Positive"
     else:
         result1 = "Negative"
-
 
 
     return render(request, 'predict.html', {"result2":result1})
